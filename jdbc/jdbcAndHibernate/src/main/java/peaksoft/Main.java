b1fc36ecb79455633bbac8485aba01531e54c1da
Zhumgalbek <jumgalbek2002@icloud.com>
2022-10-17 17:53:51 +0300

    JDBC users postgres

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,27 @@
 package peaksoft;
 
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.User;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+        UserDaoJdbcImpl userDaoJdbc = new UserDaoJdbcImpl();
+        userDaoJdbc.createUsersTable();
+//        userDaoJdbc.dropUsersTable();
+//        userDaoJdbc.saveUser("argen ","gena",(byte)8 );
+//        userDaoJdbc.saveUser("syimyk ","mena",(byte)8 );
+//        userDaoJdbc.saveUser("Kanat ","Subanov",(byte)13 );
+//        userDaoJdbc.removeUserById(1);
+//        for (User u: userDaoJdbc.getAllUsers()) {
+//            System.out.println(u);
+//        }
+//        userDaoJdbc.cleanUsersTable();
+
+
+
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,30 @@
 package peaksoft.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+//    private static final String user = "postgres";
+//    private static final String password = "1234";
+//    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+//
+//    public static Connection connect() throws SQLException {
+//
+//        return DriverManager.getConnection(url, user, password);
+//    }
+
+    public final Connection connection;
+    public Util() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "1234"
+        );
+    }
+    public  Connection getConnection() {
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
+
 }
